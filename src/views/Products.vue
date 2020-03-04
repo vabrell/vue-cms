509613edf210df9bf95ea2993579b63b05df0533
Victor Abrell <victor@abrell.se>
2020-03-04 11:02:24 +0100

    Added option to remove a product, small bug fixes * Added a button in the edit component so it's possible to remove a product. * Fixed so that if no ID have been supplied when the edit path is used, all products is still shown. * Added a alert on the success of removing a product

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,13 +1,33 @@
 <template>
-  <b-container>
+	<b-container>
+		<b-alert
+			variant="success"
+			dismissible
+			@dismissed="dismissCoutdown = 0"
+			@dismiss-count-down="countDownChange"
+			:show="dismissCountDown"
+			class="mt-3"
+		>
+			<strong>Produkten har tagits bort!</strong>
+			<b-progress
+				variant="success"
+				:max="5"
+				:value="dismissCountDown"
+				height="4px"
+				class="mt-1"
+			/>
+		</b-alert>
 
-    <AddProduct v-if="action === 'new'" />
+		<AddProduct v-if="action === 'new'" />
 
-    <EditProduct v-if="action === 'edit' && id" :id="id" />
+		<EditProduct
+			@product-deleted="productDeleted"
+			v-else-if="action === 'edit' && !isNaN(id)"
+			:id="id"
+		/>
 
-    <ShowProducts v-if="!action" />
-
-  </b-container>
+		<ShowProducts v-else />
+	</b-container>
 </template>
 
 <script>
@@ -16,17 +36,29 @@ import ShowProducts from '@/components/products/ShowProducts.vue'
 import EditProduct from '@/components/products/EditProduct.vue'
 
 export default {
-  data() {
-    return {
-      action: this.$route.params.action,
-      id: Number(this.$route.params.id)
-    }
-  },
+	data() {
+		return {
+			action: this.$route.params.action,
+			id: Number(this.$route.params.id),
+			dismissCountDown: 0
+		}
+	},
 
-  components: {
-    AddProduct,
-    EditProduct,
-    ShowProducts
-  }
+	methods: {
+		countDownChange(dismissCountDown) {
+			this.dismissCountDown = dismissCountDown
+    },
+    
+    productDeleted() {
+      this.action = null
+      this.dismissCountDown = 5
+    }
+	},
+
+	components: {
+		AddProduct,
+		EditProduct,
+		ShowProducts
+	}
 }
 </script>
