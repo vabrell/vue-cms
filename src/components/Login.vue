1b1a538ac2dfc590a4bec1e6d65cbff818b88ec8
Edwin Berg <e.berg117@gmail.com>
2020-03-18 20:16:55 +0100

    Switched the name of SignIn to Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,12 +4,13 @@
       <b-card title="Logga in">
         <b-form-group label="E-post" label-for="input-2">
           <b-form-input required placeholder="name@example.com" v-model="email" type="email"></b-form-input>
+          <p class="text-danger">{{errorMessageEmail}}</p>
         </b-form-group>
 
         <b-form-group label="Lösenord" label-for="input-2">
           <b-form-input type="password" required placeholder="Skriv in lösenord" v-model="password"></b-form-input>
+          <p class="text-danger">{{errorMessagePassword}}</p>
         </b-form-group>
-        <b-form-checkbox class="pb-2 mr-sm-2 mb-sm-0">Kom ihåg mig</b-form-checkbox>
         <b-button type="submit" variant="primary">Logga in</b-button>
         <router-link class="ml-2" to="/register">Inte registrerad?</router-link>
       </b-card>
@@ -21,13 +22,36 @@
 export default {
   data() {
     return {
+      cookie: null,
       email: null,
-      password: null
+      password: null,
+      errorMessageEmail: null,
+      errorMessagePassword: null
     };
   },
   methods: {
     onSubmit() {
-      console.log(this.email, this.password);
+      fetch("http://localhost:8080/api/users/login", {
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      })
+        .then(response => response.json())
+        .then(result => {
+          if (result.error) {
+            this.errorMessageEmail = result.messageEmail;
+            this.errorMessagePassword = result.messagePassword;
+          } else {
+            this.cookie = result.id;
+            this.$router.push("/");
+            this.$store.dispatch("getCookie");
+          }
+        });
     }
   }
 };
