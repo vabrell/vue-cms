079aaed65ddfa6a43c63366fa8e3490303b8b5d4
gabbe98 <gabriel.dahlberg1998@gmail.com>
2020-03-18 10:09:18 +0100

    Added backoffice frontend and a backend for orders

---
+++ backend/orders/index.js
@@ -0,0 +1,182 @@
+const express = require('express'),
+    path = require('path'),
+    router = express.Router(),
+    sqlite = require('sqlite')
+
+/**
+ * ****************
+ * Get all orders
+ * ****************
+ */
+router.get('/orders', async (request, response, next) => {
+    try {
+        const db = await sqlite.open(process.env.DATABASE, {
+                Promise
+            }),
+            // Get all the orders from the database
+            orders = await db.all('SELECT * FROM orders')
+
+        // Return the found orders
+        response.status(200).send(orders)
+    } catch (err) {
+        next(err)
+    }
+})
+
+/**
+ * ***************
+ * Get one order
+ * ***************
+ */
+router.get('/orders/:id', async (request, response, next) => {
+    // Try to get the order to check if it exists
+    try {
+        const db = await sqlite.open(process.env.DATABASE, {
+                Promise
+            }),
+            // Look for the product supplied
+            orders = await db.get('SELECT * FROM orders WHERE id=?', [
+                request.params.id
+            ])
+
+        // If there is no order, return an error
+        if (orders.length < 1) {
+            response.status(400).send({
+                error: true,
+                message: 'Ordern kunde inte hittas.'
+            })
+
+            // If the order exists, return the orders
+        } else {
+            response.status(200).send(orders)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+/**
+ * *****************
+ * Add a new order
+ * *****************
+ */
+
+router.post('/orders', async (request, response, next) => {
+    const errors = []
+
+    // Check if a order status is supplied
+    if (!request.body.status) {
+        errors.push({
+            field: 'status',
+            message: 'En status på ordern måste fyllas i.'
+        })
+    }
+
+    // Check if a shipping of the order have been supplied
+    if (!request.body.shipping) {
+        errors.push({
+            field: 'shipping',
+            message: 'Ett leveranssätt för produkten måste fyllas i.'
+        })
+    }
+
+    // Check if details have been supplied
+    if (!request.body.details) {
+        errors.push({
+            field: 'details',
+            message: 'Addressuppgifter måste skickas med.'
+        })
+    }
+
+    // Check if a products have been supplied
+    if (!request.body.products) {
+        errors.push({
+            field: 'products',
+            message: 'Produkter måste skickas med.'
+        })
+    }
+
+    // Check if there is any errors with the request
+    if (errors.length > 0) {
+        // Send back errors
+        response.status(400).send({
+            error: true,
+            errors: errors
+        })
+    } else {
+        try {
+            const db = await sqlite.open(process.env.DATABASE, {
+                Promise
+            })
+            // Add the order to the database
+            await db.run(
+                'INSERT INTO orders(status, shipping, products, details) VALUES(?, ?, ?, ?)',
+                [
+                    request.body.status,
+                    request.body.shipping,
+                    request.body.products,
+                    request.body.details
+                ]
+            )
+
+
+            // Return created status
+            response.status(201).send({
+                error: false,
+                message: 'Ordern har lagts till.'
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+})
+
+/**
+ * *****************
+ * Update a order
+ * *****************
+ */
+
+router.put('/orders/:id', async (request, response, next) => {
+    const errors = []
+
+    // Check if a order status is supplied
+    if (!request.body.status) {
+        errors.push({
+            field: 'status',
+            message: 'En status måste fyllas i.'
+        })
+    }
+
+    // Check if there is any errors with the request
+    if (errors.length > 0) {
+        // Send back errors
+        response.status(400).send({
+            error: true,
+            errors: errors
+        })
+    } else {
+        try {
+            const db = await sqlite.open(process.env.DATABASE, {
+                Promise
+            })
+            await db.run(
+                'UPDATE products SET status=? WHERE id=?',
+                [
+                    request.body.status,
+                ]
+            )
+
+            // Return created status
+            response.status(201).send({
+                error: false,
+                message: 'Ordern har uppdaterats.'
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+})
+
+
+module.exports = router
---
+++ src/components/Backoffice.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="mt-3">
+        <h2>Ordrar</h2>
+        <b-table class="mt-3" :fields="fields" :items="orders">
+            <template v-slot:cell(id)="data">
+                <router-link :to="`/admin/orders/edit/${data.item.id}`">{{ data.item.id }}</router-link>
+            </template>
+        </b-table>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                orders: null,
+                fields: [{
+                        id: 'Ordernummer'
+                    },
+                    {
+                        shipping: 'Leveranssätt'
+                    },
+                    {
+                        status: 'Status'
+                    }
+                ]
+            }
+        },
+        created() {
+            fetch('http://localhost:8080/api/orders')
+                .then(response => response.json())
+                .then(result => {
+                    this.orders = result
+                })
+        }
+    }
+</script>
+
+<style>
+
+</style>
