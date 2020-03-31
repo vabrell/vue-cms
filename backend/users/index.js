65a91bcdcc919c5a5a36d8f389b35e3d69d38854
Edwin Berg <e.berg117@gmail.com>
2020-03-31 14:40:50 +0200

    merge commit

--- backend/users/index.js
+++ backend/users/index.js
@@ -40,13 +40,14 @@ router.post('/users/login', async (request, response, next) => {
       // Kolla om lösenordet matchar
       const match = await bcrypt.compare(request.body.password, users[0].password)
       if (match) {
-				const user = JSON.stringify({
-					id: users[0].id,
-					admin: users[0].admin
-				})
+        const user = JSON.stringify({
+          id: users[0].id,
+          admin: users[0].admin
+        })
 
         response.set('Set-Cookie', `login=${user}; path=/`)
         response.status(201).send(users[0])
+        //om inte lösenordet stämmer överens med databasen
       } else {
         response.status(401).send({ error: true, messagePassword: 'Ogiltlig lösenord!' })
       }
@@ -85,6 +86,57 @@ router.get('/users/:id', async (request, response, next) => {
     next(err)
   }
 })
+
+/**
+ * *****************
+ * Forgotten password
+ * *****************
+ */
+
+router.post('/users/passwordreset', async (request, response, next) => {
+  try {
+    const db = await sqlite.open(process.env.DATABASE, { Promise }),
+      // Find the user with the supplied Email
+      user = await db.all('SELECT * FROM users WHERE email=?', [request.body.email])
+    if (user.length > 0) {
+      if (user[0].securityQuestion === request.body.securityQuestion && user[0].securityAnswer === request.body.securityAnswer) {
+        response.status(201).send({ error: false, user: user[0].id })
+      } else {
+        response.status(401).send({
+          error: true,
+          messageSecurityQuestion: 'Ogiltlig fråga eller svar!'
+        })
+      }
+    } else {
+      response.status(401).send({
+        error: true,
+        messageEmail: 'Ogiltlig email!'
+      })
+
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+/**
+ * *****************
+ * New password
+ * *****************
+ */
+
+router.post('/users/newpassword', async (request, response, next) => {
+  try {
+    const db = await sqlite.open(process.env.DATABASE, { Promise }),
+      // Find the user with the supplied ID
+      hashedPassword = bcrypt.hashSync(request.body.password, 10),
+      user = await db.run('UPDATE users SET password=? WHERE id=?', [hashedPassword, request.body.id])
+    response.status(201).send({ message: 'Ditt lösenord har uppdaterats!' })
+  } catch (err) {
+    next(err)
+  }
+})
+
 
 /**
  * *****************
@@ -138,6 +190,22 @@ router.post('/users', async (request, response, next) => {
     })
   }
 
+  //check if security question have been selected
+  if (!request.body.securityQuestion) {
+    errors.push({
+      field: 'securityQuestion',
+      message: 'Säkerhetsfrågan måste väljas.'
+    })
+  }
+
+  //check if security question have been answered
+  if (!request.body.securityAnswer) {
+    errors.push({
+      field: 'securityAnswer',
+      message: 'Säkerhetsfrågan måste besvaras.'
+    })
+  }
+
   // If there is any errors, send them back
   if (errors.length > 0) {
     response.status(400).send(errors)
@@ -152,8 +220,8 @@ router.post('/users', async (request, response, next) => {
       const db = await sqlite.open(process.env.DATABASE, { Promise }),
         // Add the new user
         addUser = await db.run(
-          'INSERT INTO users(name, email, password, admin) VALUES(?, ?, ?, ?)',
-          [request.body.name, request.body.email, password, admin]
+          'INSERT INTO users(name, email, password, admin, securityQuestion, securityAnswer) VALUES(?, ?, ?, ?, ?, ?)',
+          [request.body.name, request.body.email, password, admin, request.body.securityQuestion, request.body.securityAnswer]
         )
 
       response.status(201).send({
