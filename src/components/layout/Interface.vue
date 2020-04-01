0913c07745143dbd8852c38e1c0473c450de5f5d
Emil Backlund <emilbacklund@outlook.com>
2020-04-01 14:29:40 +0200

    Added back button to interface links, fixed margin

--- src/components/layout/Interface.vue
+++ src/components/layout/Interface.vue
@@ -10,35 +10,38 @@
         </router-link>
       </b-col>
       <!-- Button 2 -->
-      <b-col lg="4" md="6" xs="6" class="zoom">
+      <b-col lg="4" md="6" xs="6" class="zoom mb-5">
         <router-link to="/admin/backoffice" class="nav-link">
           <b-icon v-b-tooltip.hover title="Ordrar" font-scale="7.5" icon="archive" />
-          <div>Ordrar <b-badge :variant="variant">{{ unhandeled }}</b-badge></div>
+          <div>
+            Ordrar
+            <b-badge :variant="variant">{{ unhandeled }}</b-badge>
+          </div>
         </router-link>
       </b-col>
       <!-- Button 3 -->
-      <b-col lg="4" md="6" xs="6" class="zoom">
+      <b-col lg="4" md="6" xs="6" class="zoom mb-5">
         <router-link to="/admin/statistics" class="nav-link">
           <b-icon v-b-tooltip.hover title="Statistik" font-scale="7.5" icon="graph-up" />
           <div>Statistik</div>
         </router-link>
       </b-col>
       <!-- Button 4 -->
-      <b-col lg="4" md="6" xs="6" class="zoom">
+      <b-col lg="4" md="6" xs="6" class="zoom mb-5">
         <router-link class="nav-link" to="/admin/products">
           <b-icon v-b-tooltip.hover title="Produkter" font-scale="7.5" icon="file-richtext" />
           <div>Produkter</div>
         </router-link>
       </b-col>
       <!-- Button 5 -->
-      <b-col lg="4" md="6" xs="6" class="zoom">
+      <b-col lg="4" md="6" xs="6" class="zoom mb-5">
         <router-link class="nav-link" to="/admin/categories">
           <b-icon v-b-tooltip.hover title="Kategorier" font-scale="7.5" icon="inboxes" />
           <div>Kategorier</div>
         </router-link>
       </b-col>
       <!-- Button 6 -->
-      <b-col lg="4" md="6" xs="6" class="zoom">
+      <b-col lg="4" md="6" xs="6" class="zoom mb-5">
         <router-link class="nav-link" to="/admin/settings">
           <b-icon v-b-tooltip.hover title="Inställningar" font-scale="7.5" icon="gear-fill" />
           <div>Inställningar</div>
